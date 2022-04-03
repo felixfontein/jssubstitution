da8f45285714821d174797633c368edd24c54380
Felix Fontein <felix@fontein.de>
2022-04-03 16:53:26 +0200

    Mark quoted text in red in preview.

--- src/components/view-text-dialog/view-text-dialog.component.ts
+++ src/components/view-text-dialog/view-text-dialog.component.ts
@@ -10,17 +10,21 @@ import { SubstitutionService } from '../../app/services/substitution.service';
   styleUrls: ['./view-text-dialog.component.scss']
 })
 export class ViewTextDialogComponent {
-  public readonly letters: (string | undefined)[];
+  public readonly letters: [string | undefined, boolean][];
 
   constructor(appState: AppStateService,
               substitution: SubstitutionService) {
     this.letters = [];
     const subs = substitution.getCurrent();
+    let isQuoted = false;
     for (const letter of subs.alphabet.splitAndNormalize(appState.text, true)) {
       if (subs.alphabet.letterSet.has(letter)) {
-        this.letters.push(subs.lettersMap.get(letter)?.getValue());
+        this.letters.push([subs.lettersMap.get(letter)?.getValue(), isQuoted]);
       } else {
-        this.letters.push(letter);
+        this.letters.push([letter, isQuoted || letter === '"']);
+        if (letter === '"') {
+          isQuoted = !isQuoted;
+        }
       }
     }
   }
